a3cbf6a9d93c47af6fc316093dbf170bc674a3f0
ghulam mohi ud din <GHULAM-1>
2024-02-29 11:58:02 +0500

    styling:put colors on streaming

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,65 +1,71 @@
 "use client";
-import { useEffect, useState } from "react";
-import useSpeechRecognition from "@/hooks/useSpeechRecognition";
 import { useChat } from "ai/react";
+import { useEffect, useRef } from "react";
+import useSpeechRecognition from "@/hooks/useSpeechRecognition";
+export default function Chat() {
+  const { messages, input, handleInputChange, handleSubmit, setInput, append } =
+    useChat();
+  const { hasRecognitionSupport, isListening, startListening, text } =
+    useSpeechRecognition();
 
-export default function Sound() {
-  const [res, setRes] = useState("");
-  const {
-    hasRecognitionSupport,
-    isListening,
-    startListening,
-    stopListening,
-    text,
-  } = useSpeechRecognition();
-  const { messages, append } = useChat();
+  const formRef = useRef<HTMLFormElement>(null);
 
   useEffect(() => {
     if (!isListening && text) {
-      console.log("in the fetch req");
-      const message = {
-        role: "system",
+      console.log(text);
+      append({
         content: text,
-      };
-
-      // Perform fetch request here
-      fetch("http://localhost:3000/api/chat", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ messages: [message] }), // Send the message in the request body
-      })
-        .then((response) => response.text())
-        .then((data) => {
-          // Handle the response data here
-          console.log(data);
-          setRes(data);
-        })
-        .catch((error) => {
-          // Handle any errors here
-          console.error("Error:", error);
-        });
+        role: "user",
+      });
     }
-    
-  }, [isListening, text, append]);
+  }, [isListening, text]);
 
   return (
-    <>
-      <div>
+    <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch">
+      <div className="flex gap-4  items-center">
         {hasRecognitionSupport ? (
           <>
             <div>
-              <button onClick={startListening}>start listening</button>
+              <button
+                className="bg-red-500 rounded py-2 px-4"
+                onClick={startListening}
+              >
+                start listening
+              </button>
             </div>
-            {isListening ? <div>browser is listening</div> : null}
-            {text}
+            {isListening ? (
+              <div className="text-yellow-400"> listening ....</div>
+            ) : null}
+            {/* {text} */}
           </>
         ) : (
           <h1>no speech recognition device</h1>
         )}
       </div>
-      <div>{res}</div>
-    </>
+
+      <div className="mt-4">
+        {messages.map((m) => (
+          <div key={m.id} className="whitespace-pre-wrap mb-4">
+            {m.role === "user" ? (
+              <span className="text-white font-bold ">user : </span>
+            ) : (
+              <span className="text-green-500 font-bold">AI : </span>
+            )}
+            {m.content}
+          </div>
+        ))}
+      </div>
+
+      <form ref={formRef} onSubmit={handleSubmit}>
+        <input
+          hidden
+          className="fixed bottom-0 w-full max-w-md p-2 mb-8 border text-black border-gray-300 rounded shadow-xl"
+          value={input}
+          placeholder="Say something..."
+          onChange={handleInputChange}
+        />
+        <button type="submit" style={{ display: "none" }}></button>
+      </form>
+    </div>
   );
 }
